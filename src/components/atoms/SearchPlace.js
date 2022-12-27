15c0f23fe970ac9aa204d0aec15c86f303720734
unknown <yurim@ajou.ac.kr>
2022-12-27 15:39:24 +0900

    ResultPage

--- src/components/atoms/SearchPlace.js
+++ src/components/atoms/SearchPlace.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function SearchPlace(props){
 
@@ -13,6 +14,13 @@ function SearchPlace(props){
 
     const onFinalChange = (e) =>{
         setSelectFinal(e.target.value)
+    }
+    const movePage=useNavigate();
+
+    function clickFunc(e){
+        e.preventDefault();
+        console.log('You Clicked submit');
+        movePage('/select/another');
     }
 
     const onKeyPress=(e)=>{
@@ -32,21 +40,29 @@ function SearchPlace(props){
             
     }
 
-   
     return(
-        <div className="h-12 m-10 mx-44 bg-gray-light border border-solid border-gray-main rounded-3xl">
-            <form onSubmit={onKeyPress}>
-                <div className="flex justify-between">
-                <div className="w-full flex items-center grid grid-cols-2 divide-x divide-black">
-                    <input className="m-2 flex justify-center items-center" placeholder="어디서 출발하세요?" type="text" name="Start" value={SelectStart} onChange={onStartChange} onKeyPress={onKeyPress}/>
-                    <input className="m-2 flex justify-center" placeholder="어디로 가세요?" type="text" name="Final" value={SelectFinal} onChange={onFinalChange} onKeyPress={onKeyPress}/>
-                    </div>
-                    <button type="submit">
-                    </button>
-                </div>
-            </form>
-        </div>
+        <label class="relative block w-250">
+            <input class="placeholder:italic placeholder:text-slate-400 block w-10020 border" placeholder="출발지를 입력해주세요..." type="text" name="Start"></input>
+        </label>
     );
+
+   /*
+    return(
+        <div className="relative">
+        <div className=" h-12 m-10 mx-44 bg-gray-light border border-solid border-gray-main rounded-3xl">
+            <form className="flex justify-center items-center"onSubmit={onKeyPress}>
+                <div className="w-full top-0 flex justify-between items-center grid grid-cols-2 divide-x divide-black">
+                    <input class="m-2 flex placeholder:italic justify-center items-center" placeholder="출발지를 입력해주세요..." type="text" name="Start" value={SelectStart} onChange={onStartChange} onKeyPress={onKeyPress}/>
+                    <input class="m-2 flex placeholder:italic justify-center" placeholder="도착지를 입력해주세요..." type="text" name="Final" value={SelectFinal} onChange={onFinalChange} onKeyPress={onKeyPress}/>
+                </div>
+
+            </form>
+            <button className="static justify-between top-100 right-10 w-full" onClick={clickFunc} >검색</button>           
+        </div>
+        </div>
+        
+
+    );*/
 }
 
 export default SearchPlace;
