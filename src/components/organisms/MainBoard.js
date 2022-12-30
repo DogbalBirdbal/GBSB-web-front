a1b306f2e6bf710383bbe5e4cb8dc6d97673cd36
hanuuny <hanuuny@gmail.com>
2022-12-30 13:51:42 +0900

    change click event

--- src/components/organisms/MainBoard.js
+++ src/components/organisms/MainBoard.js
@@ -1,7 +1,22 @@
 import StartButton from "../atoms/StartButton";
 import MainIndex from "../atoms/MainIndex";
-import {NavLink} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from 'react-cookie';
+
 function MainBoard(){
+
+    const navigate = useNavigate();
+    const [cookies, setCookie, removeCookie] = useCookies(['user']);
+
+
+    const handleOnClick = () => {
+        if (cookies.user === undefined) {
+            navigate('/login');
+        } else {
+            navigate('/select/another');
+        }
+    }
+
     return(
         <>
             <div className="w-full relative">
@@ -19,9 +34,7 @@ function MainBoard(){
                                 <p className="text-4xl">
                                     : 또 나만 진심이지
                                 </p>
-                                <NavLink to="/login" style={{color: "white"}}>
-                                    <div className="text-xl font-bold border-2 rounded-lg border-white px-10 py-2 hover:bg-white hover:text-black duration-500">Start</div>
-                                </NavLink>
+                                <div className="text-xl font-bold border-2 rounded-lg border-white px-10 py-2 hover:bg-white hover:text-black duration-500" onClick={handleOnClick}>Start</div>
                                 </div>
                             </div>
                             {/*<div className="flex items-center ml-10">*/}
